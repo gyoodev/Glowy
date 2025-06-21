88de08c0ebc1ad26425a6f2c72fe58a43c946903
Gyuray Kamber <gyuraykamber99@gmail.com>
2025-06-21 09:36:31 +0000

    Mail transport update

--- src/pages/api/send-email/index.ts
+++ src/pages/api/send-email/index.ts
@@ -39,18 +39,3 @@ export function createTransport() {
  });
  return transporter;
 }
-
-function createTransport() {
- // Configure the transporter using environment variables
- const transporter = nodemailer.createTransport({
- host: process.env.SMTP_HOST,
- port: parseInt(process.env.SMTP_PORT || '587', 10), // Default to 587 if not set
- secure: process.env.SMTP_PORT === '465', // true for 465, false for other ports
- auth: {
- user: process.env.SMTP_USER,
- pass: process.env.SMTP_PASS,
- },
- });
-
- return transporter;
-}
