2ae6bf000df10457f1d9d97e822069946ccdf975
Gyuray Kamber <gyuraykamber99@gmail.com>
2025-07-01 14:32:27 +0000

    5:18:47 PM: buildbot version: c5127b7ed30083b5c3949537dc2c6164a9d943b6 5

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const pwa = withPWAInit({
   dest: "public",
   register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
 });
 
